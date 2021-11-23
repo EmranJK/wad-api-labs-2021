62de5f10dbdbcaa13f598f0980c68eb76657ff7c
Emran Sabbagh <emran2156@gmail.com>
2021-11-23 23:43:31 +0000

    Mongo lab Users Router

--- movies-api/index.js
+++ movies-api/index.js
@@ -3,7 +3,7 @@ import express from 'express';
 import moviesRouter from './api/movies';
 import './db';
 import './seedData';
-
+import usersRouter from './api/users';
 
 dotenv.config();
 
@@ -18,3 +18,6 @@ app.use('/api/movies', moviesRouter);
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
 });
+
+//Users router
+app.use('/api/users', usersRouter);
